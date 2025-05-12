c62748f764575f09ca437fd47fb3a28c0775d29d
Jan Michek <jan@janmichek.cz>
2025-05-12 13:19:56 +0700

    2025 update

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,32 +1,23 @@
 module.exports = {
-  'plugins': [
-    'stylelint-prettier',
-  ],
-  'extends': [
+  defaultSeverity: 'warning',
+  extends: [
     'stylelint-config-standard',
-    'stylelint-prettier/recommended',
+    'stylelint-config-recommended',
+    'stylelint-config-html/vue',
+    'stylelint-config-recommended-vue',
   ],
-  'defaultSeverity': process.env.NODE_ENV === 'development' ? 'warning' : 'error',
-  'rules': {
-    'prettier/prettier': true,
-    'block-opening-brace-space-before': 'always',
-    'selector-list-comma-newline-after': 'always',
-    'block-opening-brace-newline-after': 'always',
-    'block-closing-brace-newline-before': 'always',
-    'block-closing-brace-newline-after': 'always',
-    'declaration-block-trailing-semicolon': 'always',
-    'declaration-colon-newline-after': 'always-multi-line',
-    'declaration-block-semicolon-newline-after': 'always',
+  rules: {
     'declaration-block-no-duplicate-properties': true,
-    'selector-pseudo-element-colon-notation': 'double',
-    'declaration-colon-space-after': 'always',
-    'declaration-colon-space-before': 'never',
+    'selector-pseudo-element-colon-notation': 'single',
     'comment-whitespace-inside': null,
     'comment-empty-line-before': null,
+    'rule-empty-line-before': null,
+    'selector-class-pattern': null,
+    'declaration-block-no-redundant-longhand-properties': null,
     'declaration-empty-line-before': [
       'never',
       {
-        'ignore': [
+        ignore: [
           'after-comment',
           'after-declaration',
         ],
@@ -35,25 +26,11 @@ module.exports = {
     'max-nesting-depth': [
       3,
       {
-        'ignore': [
-          'blockless-at-rules',
+        ignore: [
           'pseudo-classes',
+          'blockless-at-rules',
         ],
       },
-    ],
-    'selector-pseudo-class-no-unknown': [
-      true,
-      {
-        'ignorePseudoClasses': [
-          'global',
-        ],
-      },
-    ],
-    'selector-pseudo-element-no-unknown': [
-      true,
-      {
-        ignorePseudoElements: ['v-deep']
-      }
     ]
   },
-};
+}
